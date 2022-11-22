b0e1b0d3245ae381058f6030a02aec404fb798fc
0xKent <kentw8888@gmail.com>
2022-11-22 22:35:28 +0800

    feat: add MongoDB & Mongoose

--- app.js
+++ app.js
@@ -4,20 +4,37 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 const generateUrl = require('./generate_url')
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 //setting template engine
 app.engine('hbs', engine({ defaultLayout: 'main', extname: ".hbs" }))
 app.set('view engine', 'hbs')
 
 //setting body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 //setting routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.post('/', (req, res) => {
-  const newUrl =generateUrl(req.body)
-  res.render('index', {newUrl})
+  const newUrl = generateUrl(req.body)
+  res.render('index', { newUrl })
 
 })
 
